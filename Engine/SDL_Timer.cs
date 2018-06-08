00ec91543a66145b3b0867008d791a75092efef4
Mladen Stanišić <lordstanius@gmail.com>
2018-06-08 17:50:13 +0200

    Timer refactored

--- Engine/SDL_Timer.cs
+++ Engine/SDL_Timer.cs
@@ -14,9 +14,6 @@ namespace Digger.Net
             double tfreq = 1000000.0 / FrameTime;
             loop_error = Math.recfilter_init(tfreq, 0.1);
             Math.PFD_init(ref phase_detector, 0.0);
-#if DIGGER_DEBUG
-            Log.Write($"inittimer: ftime = {FrameTime}");
-#endif
         }
 
         public uint FrameTime { get; set; }
@@ -26,21 +23,16 @@ namespace Digger.Net
             if (FrameTime <= 1)
                 return;
 
-            double tfreq = 1000000.0 / FrameTime;
-            double clk_rl = SDL.SDL_GetTicks() * tfreq / 1000.0;
+            double fps = 1000000.0 / FrameTime;
+            double clk_rl = SDL.SDL_GetTicks() * fps / 1000.0;
             double eval = Math.PFD_get_error(ref phase_detector, clk_rl);
-            double filterval;
-            if (eval != 0)
-                filterval = Math.recfilter_apply(ref loop_error, Math.sigmoid(eval));
-            else
-                filterval = Math.recfilter_getlast(ref loop_error);
+            double filterval = eval != 0 ?
+                Math.recfilter_apply(ref loop_error, Math.sigmoid(eval)):
+                Math.recfilter_getlast(ref loop_error);
 
-            double add_delay_d = (Math.freqoff_to_period(tfreq, 1.0, filterval) * 1000.0) + cum_error;
+            double add_delay_d = (Math.freqoff_to_period(fps, 1.0, filterval) * 1000.0) + cum_error;
             uint add_delay = (uint)System.Math.Round(add_delay_d);
             cum_error = add_delay_d - add_delay;
-#if DIGGER_DEBUG
-            Log.Write($"clk_rl = {clk_rl}, add_delay = {add_delay}, eval = {eval}, filterval = {filterval}, cum_error = {cum_error}");
-#endif
             SDL.SDL_Delay(add_delay);
         }
     }
